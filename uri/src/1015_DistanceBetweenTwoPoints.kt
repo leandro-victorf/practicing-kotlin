52264a386ea5b28d1c2ecc9100ead17954053e34
leandro-victorf <leandro.victorf@gmail.com>
2021-09-30 16:41:58 -0300

    Creat Distance exercise 1016

--- uri/src/1015_DistanceBetweenTwoPoints.kt
+++ uri/src/1015_DistanceBetweenTwoPoints.kt
@@ -1,2 +1,19 @@
+import java.util.*
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class `1015_DistanceBetweenTwoPoints` {
+
+
+    fun main(args: Array<String>) {
+        val leitor = Scanner(System.`in`)
+        val x1 = leitor.nextDouble()
+        val y1 = leitor.nextDouble()
+        val x2 = leitor.nextDouble()
+        val y2 = leitor.nextDouble()
+        val distance = sqrt((x1 - x2).pow(2.0) + (y1 - y2).pow(2.0))
+
+        println(String.format("%.4f", distance))
+
+    }
 }
---
+++ uri/src/1016_Distance.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+class `1016_Distance` {
+
+    fun main(args: Array<String>) {
+        val leitor = Scanner(System.`in`)
+        val speed = leitor.nextInt()
+
+        println("${speed * 2} minutos")
+    }
+}
