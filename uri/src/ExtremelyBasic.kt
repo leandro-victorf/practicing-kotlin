ee262b5ba4c82d2d209e98207805a2c7ca9f2e16
leandro-victorf <leandro.victorf@gmail.com>
2021-09-30 14:22:26 -0300

    Creat Extremely Basic exercise 1001

---
+++ uri/src/ExtremelyBasic.kt
@@ -0,0 +1,12 @@
+class ExtremelyBasic {
+    import java.util.*
+
+    fun main(args: Array<String>) {
+        val leitor = Scanner(System.`in`)
+        val termoUm = leitor.nextInt()
+        val termoDois = leitor.nextInt()
+
+        println("X = ${termoUm + termoDois}")
+
+    }
+}
