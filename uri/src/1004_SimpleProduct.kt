b63cb70f2c5fe0aac0327fe3242ec82158a5aa9b
leandro-victorf <leandro.victorf@gmail.com>
2021-09-30 15:47:34 -0300

    Creat Simple Product exercise 1004

---
+++ uri/src/1004_SimpleProduct.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+class `1004_SimpleProduct` {
+
+    fun main(args: Array<String>) {
+        val leitor = Scanner(System.`in`)
+        val termoUm = leitor.nextInt()
+        val termoDois = leitor.nextInt()
+
+        println("PROD = ${termoUm * termoDois}")
+
+    }
+}
