8a14542e25954ab4d744dcdc09711a3319b606c1
leandro-victorf <leandro.victorf@gmail.com>
2021-09-30 15:43:54 -0300

    Creat Simple Sum exercise 1003

---
+++ uri/src/1003_SimpleSum.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+class `1003_SimpleSum` {
+
+    fun main(args: Array<String>) {
+        val leitor = Scanner(System.`in`)
+        val termoUm = leitor.nextInt()
+        val termoDois = leitor.nextInt()
+
+        println("SOMA = ${termoUm + termoDois}")
+
+    }
+}
