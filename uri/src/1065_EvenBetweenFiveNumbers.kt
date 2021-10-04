78170e4d540fb6e130a0f02da42fcab9f4d81ccb
leandro-victorf <leandro.victorf@gmail.com>
2021-10-04 10:57:33 -0300

    Creat Even Between five numbers exercise 1065

--- uri/src/1065_EvenBetweenFiveNumbers.kt
+++ uri/src/1065_EvenBetweenFiveNumbers.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-class `1064_PositivesAndAverage` {
+class `1065_EvenBetweenFiveNumbers` {
 
     fun main(args: Array<String>) {
         val reader = Scanner(System.`in`)
@@ -9,20 +9,16 @@ class `1064_PositivesAndAverage` {
         val numberThree = reader.nextDouble()
         val numberFour = reader.nextDouble()
         val numberFive = reader.nextDouble()
-        val numberSix = reader.nextDouble()
         var sum = 0.0
         var counter = 0
-        val arrayNumbers = listOf(numberOne, numberTwo, numberThree, numberFour, numberFive, numberSix)
+        val arrayNumbers = listOf(numberOne, numberTwo, numberThree, numberFour, numberFive)
 
 
         for (i in arrayNumbers) {
-            if (i > 0.0) {
-                sum += i
+            if (i.mod(2.0) == 0.0) {
                 counter++
             }
         }
-        val average = sum / counter
-        println("$counter valores positivos")
-        println(String.format("%.1f", average))
+        println("$counter valores pares")
     }
 }
