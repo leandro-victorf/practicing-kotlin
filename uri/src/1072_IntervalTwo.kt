235e183b402dbb4f81f47c58000be06aa28c4279
leandro-victorf <leandro.victorf@gmail.com>
2021-10-04 18:13:59 -0300

    Creat sum Interval 2 exercise 1072

---
+++ uri/src/1072_IntervalTwo.kt
@@ -0,0 +1,25 @@
+import java.util.*
+
+class `1072_IntervalTwo` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        val size = reader.nextInt()
+        var numbers = mutableListOf<Int>()
+        var counterIn = 0
+        var counterOut = 0
+
+        for(i  in 0 until size){
+            numbers[i] = reader.nextInt()
+        }
+
+        for (i in numbers) {
+            if ( i >= 10 && i <= 20){
+                counterIn++
+            }else{
+                counterOut++
+            }
+        }
+        println("$counterIn in")
+        println("$counterOut out")
+    }
+}
