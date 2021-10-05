84c5e18025a62ee9172cba3481a623b34d198dfc
leandro-victorf <leandro.victorf@gmail.com>
2021-10-05 09:52:17 -0300

    Creat even square exercise 1073

---
+++ uri/src/1073_EvenSquare.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+class `1073_EvenSquare` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        var number = reader.nextInt()
+
+        if (number.mod(2) == 1) number++
+
+        for (i in 2..number) println("$i^$i = ${i*i}")
+    }
+}
