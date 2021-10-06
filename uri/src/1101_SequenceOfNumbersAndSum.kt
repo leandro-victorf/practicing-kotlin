7ddc11f1af301ce4f679b3434a8e166b76cea0db
leandro-victorf <leandro.victorf@gmail.com>
2021-10-06 11:22:33 -0300

    Creat sequence of numbers and sum exercise 1101

---
+++ uri/src/1101_SequenceOfNumbersAndSum.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+class `1101_SequenceOfNumbersAndSum` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        val numberOne = reader.nextInt()
+        val numberTwo = reader.nextInt()
+        var sum = 0
+        if (numberOne != 0 && numberTwo != 0) {
+
+            for (i in numberTwo..numberOne) {
+                print("$i ")
+                sum += i
+            }
+            println("Sum=$sum")
+        }
+    }
+}
