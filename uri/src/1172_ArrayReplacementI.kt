4986f67a94fc4bf578d776cd8917ebe94a02f004
leandro-victorf <leandro.victorf@gmail.com>
2021-10-07 17:14:32 -0300

    Fix array exercise 1172

--- uri/src/1172_ArrayReplacementI.kt
+++ uri/src/1172_ArrayReplacementI.kt
@@ -4,16 +4,13 @@ class `1172_ArrayReplacementI` {
     fun main(args: Array<String>) {
         val reader = Scanner(System.`in`)
         var numbers = mutableListOf<Int>()
-        var counterIn = 0
-        var counterOut = 0
 
         for (i in 0 until 10) {
             numbers.add(i, reader.nextInt())
+            if (numbers[i] <= 0) {
+                numbers[i] = 1
+            }
+            println("X[$i] = ${numbers[i]}")
         }
-
-        for (i in 0 until 10) {
-            println("$numbers")
-        }
-
     }
 }
