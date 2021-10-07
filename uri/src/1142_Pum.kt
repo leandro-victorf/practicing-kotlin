7390d34adc67a5006b10a63a185791982f5b2d95
leandro-victorf <leandro.victorf@gmail.com>
2021-10-07 13:42:47 -0300

    Creat PUM exercise 1142

---
+++ uri/src/1142_Pum.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+class `1142_Pum` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        val numberPrints = reader.nextInt()
+
+        for (i in 1..(4 * numberPrints)) {
+            if (i.mod(4) == 0) {
+                println("PUM")
+            } else {
+                println("$i ")
+            }
+        }
+    }
+}
