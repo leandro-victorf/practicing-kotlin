bd7471c7f29238d9c857528db9bc755565a8de7b
leandro-victorf <leandro.victorf@gmail.com>
2021-10-11 13:52:35 -0300

    Creat arrau change  I exercise 1175

---
+++ uri/src/1175_ArrayChangeI.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+class `1175_ArrayChangeI` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        var numberArray = mutableListOf<Int>()
+
+        for (i in 0..19) {
+            numberArray.add(i, reader.nextInt())
+        }
+
+        for (i in 19 downTo 0) {
+            println("N[ ${19 - i} ] = ${numberArray[i]}")
+        }
+    }
+}
