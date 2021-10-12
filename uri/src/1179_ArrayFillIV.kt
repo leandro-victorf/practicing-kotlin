137539f1001565dc32a7310750669f6a91848d81
leandro-victorf <leandro.victorf@gmail.com>
2021-10-12 16:48:56 -0300

    Creat Array Fill IV exercise 1179

---
+++ uri/src/1179_ArrayFillIV.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+class `1179_ArrayFillIV` {
+    fun main (args: Array<String>){
+        val reader = Scanner(System.`in`)
+        var numbers = mutableListOf<Int>()
+        var evenNumbers = mutableListOf<Int>()
+        var oddNumbers = mutableListOf<Int>()
+
+        for (i in 1..15){
+            numbers.add(reader.nextInt())
+            
+        }
+
+    }
+}
