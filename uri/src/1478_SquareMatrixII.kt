59eb2fee4e8adb378354d6fb34ddab919b589eae
leandro-victorf <leandro.victorf@gmail.com>
2021-10-14 17:01:58 -0300

    Creat Square Matrix II exercise 1478

---
+++ uri/src/1478_SquareMatrixII.kt
@@ -0,0 +1,22 @@
+import java.util.*
+import kotlin.math.abs
+
+class `1478_SquareMatrixII` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+
+        for (n in 1..5) {
+            var sizeMatrix = reader.nextInt()
+            if (sizeMatrix == 0) print("")
+            else {
+                for (i in 0 until sizeMatrix) {
+                    for (j in 0 until sizeMatrix) {
+                        if ( j == (sizeMatrix-1)) println("${abs(i - j) + 1}")
+                        else print("${abs(i - j) + 1}")
+                    }
+                    println()
+                }
+            }
+        }
+    }
+}
