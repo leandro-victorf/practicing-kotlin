c90eac02c4584b93910299bf07e7535aa5290684
leandro-victorf <leandro.victorf@gmail.com>
2021-10-16 11:35:21 -0300

    Creat left area exercise 1189

---
+++ uri/src/1189_LeftArea.kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+class `1189_LeftArea` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        val operation = reader.next()
+        val arrayNumbers = mutableListOf<MutableList<Double>>()
+        var sum = 0.0
+
+        for (i in 0..11) {
+            arrayNumbers.add(mutableListOf())
+            for (j in 0..11) {
+                arrayNumbers[i].add(reader.nextDouble())
+            }
+        }
+
+        for (i in 1..10) {
+            if (i < 6) {
+                for (j in 0..i) {
+                    sum += arrayNumbers[i][j]
+                }
+            } else {
+                for (j in 0..(11 - i)) {
+                    sum += arrayNumbers[i][j]
+                }
+            }
+        }
+
+        if (operation.equals("S"))
+            println("${String.format("%.1f", sum)}")
+        else
+            println("${String.format("%.1f", (sum / 30))}")
+    }
+}
--- uri/src/1478_SquareMatrixII.kt
+++ uri/src/1478_SquareMatrixII.kt
@@ -4,21 +4,19 @@ import kotlin.math.abs
 class `1478_SquareMatrixII` {
     fun main(args: Array<String>) {
         val reader = Scanner(System.`in`)
+        var sizeMatrix = reader.nextInt()
 
-        for (n in 1..5) {
-            var sizeMatrix = reader.nextInt()
-            if (sizeMatrix == 0) print("")
-            else {
-                for (i in 0 until sizeMatrix) {
-                    for (j in 0 until sizeMatrix) {
-                        if (j == (sizeMatrix - 1)) println("${abs(i - j) + 1}")
-                        else print("${abs(i - j) + 1} ")
+        while (sizeMatrix > 0) {
 
-                        if (i == (sizeMatrix - 1) && j == (sizeMatrix - 1)) println()
-                    }
-                    println()
+            for (i in 0 until sizeMatrix) {
+                for (j in 0 until sizeMatrix) {
+                    print("  ${abs(i - j) + 1}")
+                    if (i == (sizeMatrix - 1) && j == (sizeMatrix - 1)) println()
+                    else print("")
                 }
+                println()
             }
+            sizeMatrix = reader.nextInt()
         }
     }
 }
