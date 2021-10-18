270275eeb75a0cd1eff3569d527e7837c8912ffa
leandro-victorf <leandro.victorf@gmail.com>
2021-10-18 13:54:15 -0300

    Fix exercise 1101

--- uri/src/1101_SequenceOfNumbersAndSum.kt
+++ uri/src/1101_SequenceOfNumbersAndSum.kt
@@ -4,19 +4,28 @@ class `1101_SequenceOfNumbersAndSum` {
     fun main(args: Array<String>) {
 
         val reader = Scanner(System.`in`)
+        var numberOne: Int
+        var numberTwo: Int
+        var aux: Int
 
-        for (j in 1..3) {
-            var numberOne = reader.nextInt()
-            var numberTwo = reader.nextInt()
+        do {
+            numberOne = reader.nextInt()
+            numberTwo = reader.nextInt()
             var sum = 0
 
-            if (numberTwo > 0) {
-                for (i in numberTwo..numberOne) {
+            if (numberOne > numberTwo) {
+                aux = numberTwo
+                numberTwo = numberOne
+                numberOne = aux
+            }
+
+            if (numberOne > 0 && numberTwo > 0) {
+                for (i in numberOne..numberTwo) {
                     print("$i ")
                     sum += i
                 }
                 println("Sum=$sum")
             }
-        }
+        } while (numberOne > 0 && numberTwo > 0)
     }
 }
