c65fde5f6f7da301696c54b0459a4bdddc25ade9
leandro-victorf <leandro.victorf@gmail.com>
2021-10-25 11:39:01 -0300

    Creat summing consecutive integers exercise 1149

--- uri/src/1149_SummingConsecutiveIntegers.kt
+++ uri/src/1149_SummingConsecutiveIntegers.kt
@@ -1,2 +1,19 @@
+import java.util.*
+
 class `1149_SummingConsecutiveIntegers` {
+    fun main(args: Array<String>) {
+        val reader = Scanner(System.`in`)
+        val number = reader.nextInt()
+        var sizesequence = reader.nextInt()
+        var sum = 0
+
+        if (sizesequence <= 0) {
+            sizesequence = reader.nextInt()
+        } else {
+            for (i in number until (number + sizesequence)) {
+                sum += i
+            }
+            println(sum)
+        }
+    }
 }
--- uri/src/1478_SquareMatrixII.kt
+++ uri/src/1478_SquareMatrixII.kt
@@ -1,7 +1,7 @@
 import java.util.*
 import kotlin.math.abs
 
-class `1478_SquareMatrixII` {
+//class `1478_SquareMatrixII` {
     fun main(args: Array<String>) {
         val reader = Scanner(System.`in`)
         var sizeMatrix = reader.nextInt()
@@ -11,12 +11,12 @@ class `1478_SquareMatrixII` {
             for (i in 0 until sizeMatrix) {
                 for (j in 0 until sizeMatrix) {
                     print("  ${abs(i - j) + 1}")
-                    if (i == (sizeMatrix - 1) && j == (sizeMatrix - 1)) println()
-                    else print("")
+                    if (j != (sizeMatrix - 1)) print(" ")
                 }
                 println()
             }
-            sizeMatrix = reader.nextInt()
+            sizeMatrix =  reader.nextInt()
+            if (sizeMatrix > 0) println()
         }
     }
-}
+//}
